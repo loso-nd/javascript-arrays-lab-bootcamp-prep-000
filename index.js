61fa498fd9b57ae481deee72e3b183f273ac377c
Nahomie <nducasse3@gmail.com>
2018-01-12 18:16:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
-  kittens.unshift(name)
+  kittens.unshift()
   return kittens
 }
