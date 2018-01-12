f2f5a1a7c07ee66b1f6aa49b6321ad5bb8c7d98d
Nahomie <nducasse3@gmail.com>
2018-01-12 18:51:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
+
+function removeLastKitten(){
+  return kittens.slice(-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
